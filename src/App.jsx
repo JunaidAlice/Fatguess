5af710c43e99ae6d3895f4381efe6be8022ab555
JunaidAlice <maskedwolf211@gmail.com>
2025-01-15 00:52:01 +0500

    Fix import statements and clean up Navbar rendering in App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,15 @@ import {
   Route,
   useLocation,
 } from "react-router-dom";
-import Navbar from "./components/Navbar";
+
 import TermsOfService from "./Pages/TermsOfService";
 import PrivacyPolicy from "./Pages/PrivacyPolicy";
 import Activity from "./Pages/Activity";
 import Rank from "./Pages/Rank";
-import Comments from "./Pages/Coments"; // Fixed spelling
-
+import Comments from "./Pages/Coments"; 
 import LandingPage from "./components/LandingPage";
-
-import Predictionboard from "./Pages/Predictionboard"; // Fixed import
-
+import Predictionboard from "./Pages/Predictionboard"; 
+import Navbar from './Components/Navbar'
 const App = () => {
   return (
     <Router>
@@ -23,23 +21,16 @@ const App = () => {
     </Router>
   );
 };
-
 const MainContent = () => {
   const location = useLocation();
-
-  // Hide Navbar on /connect and /signup
-
-  return (
+   return (
     <div>
-      {/* Conditionally render Navbar based on pathname */}
       <Navbar />
-      {/* <Dashboard/>       */}
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/activity" element={<Activity />} />
         <Route path="/rank" element={<Rank />} />
         <Route path="/comments" element={<Comments />} />
-
         <Route path="/predictionboard" element={<Predictionboard />} />
         <Route path="/terms-of-service" element={<TermsOfService />} />
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
