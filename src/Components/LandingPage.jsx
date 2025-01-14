db7d2e733476ff269549dc10c3199494122b8962
JunaidAlice <maskedwolf211@gmail.com>
2025-01-15 01:28:48 +0500

    Fix import path for FAQs component in LandingPage.jsx

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Hero from "./Hero";
+import FAQs from "./FAQs";
 import Footer from "./Footer";
-import FAQs from '../Components/FAQs'
 
 const LandingPage = () => {
   return (
