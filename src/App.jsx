c52dc0ebbbcca1c2b51d656b1b3eaea8041de332
JunaidAlice <maskedwolf211@gmail.com>
2025-01-15 01:33:37 +0500

    Fix import path for LandingPage component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import Rank from "./Pages/Rank";
 import Comments from "./Pages/Coments"; 
 
 import Predictionboard from "./Pages/Predictionboard";
- import LandingPage from "./components/LandingPage";
+ import LandingPage from "./Components/LandingPage";
 import Navbar from './Components/Navbar'
 const App = () => {
   return (
