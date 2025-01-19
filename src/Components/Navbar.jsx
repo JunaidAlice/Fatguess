0cd8b73a02b53aaf030aa87bf2a5f2c32e57f6fe
JunaidAlice <maskedwolf211@gmail.com>
2025-01-19 23:13:42 +0500

    Enhance Navbar component layout by ensuring full-width bottom border for language selection items

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -243,7 +243,7 @@ export default function Navbar() {
                           onClick={() => selectLanguage(language)}
                           className="flex items-center justify-between px-4 py-2 w-full hover:bg-gray-800"
                         >
-                          <div className="flex items-center space-x-2 border-b border-gray-600">
+                          <div className="flex items-center space-x-2 border-b w-full border-gray-600">
                             <img
                               src={language.flag}
                               alt={language.name}
